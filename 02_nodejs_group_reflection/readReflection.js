a3fc031130ac14165f0b3e5926be3a70a978d033
romeyiow <jimperialyosh@gmail.com>
2025-02-20 16:20:16 +0800

    Done with Assignment #2

--- 02_nodejs_group_reflection/readReflection.js
+++ 02_nodejs_group_reflection/readReflection.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 
 console.log("Reading the file...");
-
 fs.readFile('reflection.txt', 'utf8', (err, data) => {
     if (err) throw err;
-    console.log("\nFile content:\n", data);
+    console.log("\n========FILE CONTENT========\n", data);
 });
-
-console.log("\nDone reading the file.");
+console.log("Done reading the file.");
